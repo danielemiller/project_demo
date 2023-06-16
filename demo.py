dbe72a6c98d27c64e0c7e6b70df0d4a76ad199e8
danielemiller <danielemiller@live.com>
2023-06-16 03:58:52 +0000

    Updated script to be more complex

--- demo.py
+++ demo.py
@@ -1,10 +1,19 @@
 class DemoClass:
     def __init__(self, name):
         self.name = name
+        self.greetings = []
 
-    def greet(self):
-        return f'Hello, {self.name}!'
+    def greet(self, greeting):
+        self.greetings.append(greeting)
+        return f'{greeting}, {self.name}!'
+
+    def show_greetings(self):
+        for greeting in self.greetings:
+            print(f'{greeting}, {self.name}!')
 
 if __name__ == '__main__':
     demo = DemoClass('ChatGPT')
-    print(demo.greet())
+    demo.greet('Hello')
+    demo.greet('Hi')
+    demo.greet('Hey')
+    demo.show_greetings()
