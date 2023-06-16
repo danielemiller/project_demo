17aa06094ab3ea0ded05d8854d3aab54bfaee8e2
danielemiller <danielemiller@live.com>
2023-06-16 04:16:22 +0000

    Added SMTP email function

--- demo.py
+++ demo.py
@@ -1,1 +1,35 @@
-class DemoClass:\n    def __init__(self, name):\n        self.name = name\n        self.greetings = []\n\n    def greet(self, greeting):\n        self.greetings.append(greeting)\n        return f'{greeting}, {self.name}!'\n\n    def show_greetings(self):\n        for greeting in self.greetings:\n            print(f'{greeting}, {self.name}!')\n\nif __name__ == '__main__':\n    demo = DemoClass('ChatGPT')\n    demo.greet('Hello')\n    demo.greet('Hi')\n    demo.greet('Hey')\n    demo.show_greetings()
+import smtplib
+from email.mime.text import MIMEText
+
+class DemoClass:
+    def __init__(self, name):
+        self.name = name
+        self.greetings = []
+
+    def greet(self, greeting):
+        self.greetings.append(greeting)
+        return f'{greeting}, {self.name}!'
+
+    def show_greetings(self):
+        for greeting in self.greetings:
+            print(f'{greeting}, {self.name}!')
+
+    def send_email(self, smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        msg = MIMEText(body)
+        msg['Subject'] = subject
+        msg['From'] = from_addr
+        msg['To'] = to_addr
+
+        server = smtplib.SMTP(smtp_server, port)
+        server.starttls()
+        server.login(username, password)
+        server.send_message(msg)
+        server.quit()
+
+if __name__ == '__main__':
+    demo = DemoClass('ChatGPT')
+    demo.greet('Hello')
+    demo.greet('Hi')
+    demo.greet('Hey')
+    demo.show_greetings()
+    demo.send_email('smtp.example.com', 587, 'username', 'password', 'from@example.com', 'to@example.com', 'Hello', 'This is the body of the email.')
